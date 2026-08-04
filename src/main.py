18e055fc637fb550ad9125d45af3b1bcb5a6fe15
Hirotoshi Uchida <uho02741358@gmail.com>
2026-08-04 13:31:41 +0900

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+print("Hello world!")
+a = 5; b = 3; c = a + b
+print(c)
+total = 123 \
+      + 456 \
+      + 789
+months = [ 'Jan', 'Feb', 'Mar', 'Apr',
+           'May', 'Jun', 'Jul', 'Aug',
+           'Sep', 'Oct', 'Nov', 'Dec' ]
+a = 3
+if a == 5:
+    print("AAA")
+    print("BBB")
+print("CCC")
+print(3)
+print([1, 2, 3])
+print((1, 2, 3))
+print({'k1':10, 'k2':20})
+print("AAA", "BBB")
+print("AAA", end="")
+print("BBB")
+print("My name is %s." % "Tanaka")
+print("%s is %d years old." % ("Tanaka", 28))
+print("%(name)s is %(age)d years old." % {'name': "Tanaka", 'age': 28})
